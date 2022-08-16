21f4bdfb1da207be91639376137e4df7ea29e374
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-08-16 14:58:02 -0300

    Deploy do Projeto Blog Pessoal

--- blog-pessoal/blogpessoal56/src/main/java/com/blogpessoal/security/UserDetailsImpl.java
+++ blog-pessoal/blogpessoal56/src/main/java/com/blogpessoal/security/UserDetailsImpl.java
@@ -1,7 +1,6 @@
 package com.blogpessoal.security;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,49 +14,55 @@ public class UserDetailsImpl implements UserDetails {
 	private String userName;
 	private String password;
 	
-	private List<GrantedAuthority> authorities;
-	
 	public UserDetailsImpl(Usuario user) {
 		this.userName = user.getUsuario();
 		this.password = user.getSenha();
 	}
 	
 	public UserDetailsImpl() {}
-		
-		@Override
-		public Collection<? extends GrantedAuthority> getAuthorities() {
-			return authorities;
-		}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-		@Override
-		public String getPassword() {
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
 		return password;
-		}
-		
-		@Override
-		public String getUserName() {
-		return userName;
-		}
-		
-		@Override
-		public boolean isAccountNonExpired() {
-		return true;
-		}
-		
-		@Override
-		public boolean isAccountNonLocked() {
-		return true;
-		}
-		
-		@Override
-		public boolean isCredentialsNonExpired() {
-			return true;
-		}
+	}
 
-		@Override
-		public boolean isEnabled() {
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
 		return true;
-		}
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
 	
 	
 }
